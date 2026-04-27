10ff94cebbd5ce7700d2d5e2929a82a095cd4fed
martirale <hola@alemartir.com>
2026-04-27 03:28:52 -0600

    Adjust max-w

--- packages/admin/src/components/Header.tsx
+++ packages/admin/src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function HeaderFixed({
     <div
       className={clsx(
         'bg-background fixed top-0 w-full z-50 pt-4 h-18',
-        sidebar ? 'max-w-274 2xl:max-w-312' : 'max-w-312',
+        sidebar ? 'max-w-[52.1rem] xl:max-w-274 2xl:max-w-312' : 'max-w-312',
       )}
     >
       {children}
