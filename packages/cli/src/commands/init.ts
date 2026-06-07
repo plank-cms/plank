8311020802e9b00436a970280bada237ad5f701b
martirale <hola@alemartir.com>
2026-06-07 15:37:59 -0600

    0.30.1

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -13,7 +13,7 @@ import {
   type PackageManagerName,
 } from '../packageManager.js'
 
-const PACKAGE_VERSION = '0.30.0'
+const PACKAGE_VERSION = '0.30.1'
 
 function generateSecret(): string {
   return randomBytes(32).toString('hex')
